5b558a4c9bf97273bbebc288d1f8b5e3fd39037d
angel <aarrieta@iesrafaelalberti.es>
2024-01-17 16:23:26 +0100

    Ejercicio 10 a medias

--- Ejercicio_10.kt
+++ Ejercicio_10.kt
@@ -1,3 +1,48 @@
+class Libro(private var titulo:String, private var autor: String){
+    private var cantPaginas:Int = 1
+    private var calificacion: Int = 5
+    init {
+        require(cantPaginas > 0) { "Debe tener al menos 1 página" }
+        require( (calificacion >= 0) and (calificacion <= 10) ) { "Debe valorarse entre el 0 al 10" }
+    }
+    constructor (titulO: String, autoR: String, cantPaginaS: Int): this(titulO, autoR){
+        cantPaginas = cantPaginaS
+    }
+    constructor (titulO: String, autoR: String, cantPaginaS: Int, calificacioN: Int): this(titulO, autoR){
+        cantPaginas = cantPaginaS
+        calificacion = calificacioN
+    }
+    fun cambioTitulo(nuevoTitulo: String){
+        titulo = nuevoTitulo
+    }
+    fun cambioAutor(nuevoAutor: String){
+         autor = nuevoAutor
+    }fun cambioPaginas(nuevaNumeracion: Int){
+        cantPaginas = nuevaNumeracion
+    }
+    fun cambioCalificacion(nuevaCalificacion: Int){
+        calificacion = nuevaCalificacion
+    }
+    fun titulo(): String{
+        return titulo
+    }
+    fun autor(): String {
+        return autor
+    }
+    fun cantPaginas(): Int {
+        return cantPaginas
+    }
+    fun calificacion(): Int{
+        return calificacion
+    }
+}
+class ConjuntoLibros () {
+    private var conjunto: Array<Libro> = arrayOf()
+    constructor (conjuntO: Array<Libro>): this(){
+        conjunto = conjuntO
+    }
+}
 fun main() {
-
+    val libroFiccion = Libro("papa", "pepe", 7)
+    println(libroFiccion.calificacion())
 }
