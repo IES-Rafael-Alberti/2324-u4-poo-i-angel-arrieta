120083df8be292ca46dd7ba181be8b240fa4c85b
angel <aarrieta@iesrafaelalberti.es>
2024-01-19 09:49:26 +0100

    Ejercicio 10 finalizado

--- Ejercicio_10.kt
+++ Ejercicio_10.kt
@@ -29,7 +29,6 @@ class Libro(private var titulo:String, private var autor: String){
     }fun calificacion(): Int{
         return calificacion
     }
-
     override fun toString(): String {
         return "Titulo: $titulo.\nAutor: $autor.\n" +
                 "Cantidad de páginas: $cantPaginas.\n" +
@@ -38,7 +37,7 @@ class Libro(private var titulo:String, private var autor: String){
     }
 }
 class ConjuntoLibros () {
-    var conjunto: Array<Libro> = arrayOf()
+    private var conjunto: Set<Libro> = setOf()
     companion object{
         private val cantidadMaxLibros: Int = 5
     }
@@ -47,76 +46,106 @@ class ConjuntoLibros () {
         require(conjunto.size >= 0){ "No es posible eliminar más libros del conjunto" }
     }
     constructor (libro: Libro): this(){
-        conjunto =  arrayOf(libro)
+        conjunto =  setOf(libro)
     }constructor (libro1: Libro, libro2: Libro): this(){
-        conjunto =  arrayOf(libro1, libro2)
+        conjunto =  setOf(libro1, libro2)
     }constructor (libro1: Libro, libro2: Libro, libro3: Libro): this(){
-        conjunto =  arrayOf(libro1, libro2, libro3)
+        conjunto =  setOf(libro1, libro2, libro3)
     }constructor (libro1: Libro, libro2: Libro, libro3: Libro, libro4: Libro): this(){
-        conjunto =  arrayOf(libro1, libro2, libro3, libro4)
+        conjunto =  setOf(libro1, libro2, libro3, libro4)
     }constructor (libro1: Libro, libro2: Libro, libro3: Libro, libro4: Libro, libro5: Libro): this(){
-        conjunto =  arrayOf(libro1, libro2, libro3, libro4, libro5)
+        conjunto =  setOf(libro1, libro2, libro3, libro4, libro5)
     }
     fun anadirLibro(libroNuevo: Libro){
         conjunto += libroNuevo
     }
-    fun quitarPorTitulo(tituloLibro: String): String{
+    private fun quitarDelConjunto (librosAQuitar: MutableList<Libro>){
+        var diferencia: Int = 0
+            for (libro in librosAQuitar){
+                conjunto = conjunto.filter { it !in setOf(libro) }.toSet()
+                diferencia += 1
+            }
+    } fun quitarPorTitulo(tituloLibro: String): String{
         var accionRealizada: Boolean = false
-        var librosQueQuitar: List<Int> = listOf()
+        val librosQueQuitar: MutableList<Libro> = mutableListOf()
         for (libro in conjunto){
             if(libro.titulo() == tituloLibro){
-                librosQueQuitar += conjunto.indexOf(libro)
+                librosQueQuitar.add(libro)
                 accionRealizada = true
             }
         }
         if ( accionRealizada ) {
             val mensaje: String = if (librosQueQuitar.size > 1) { "Libros quitados de la colección" }
             else { "Libro quitado de la colección" }
-            var diferencia: Int = 0
-            for (indiceLibro in librosQueQuitar){
-                conjunto.drop(indiceLibro-diferencia)
-                diferencia += 1
-            }
+            quitarDelConjunto(librosQueQuitar)
             return mensaje
         } else {
             return "No se ha encontrado ningún libro con el título especificado"
         }
-
-    }
-    fun quitarPorAutor(autorLibro: String): String{
+    } fun quitarPorAutor(autorLibro: String): String{
         var accionRealizada: Boolean = false
-        var librosQueQuitar: List<Int> = listOf()
+        val librosQueQuitar: MutableList<Libro> = mutableListOf()
         for (libro in conjunto){
             if(libro.autor() == autorLibro){
-                librosQueQuitar += conjunto.indexOf(libro)
+                librosQueQuitar.add(libro)
                 accionRealizada = true
             }
         }
         if ( accionRealizada ) {
             val mensaje: String = if (librosQueQuitar.size > 1) { "Libros quitados de la colección" }
             else { "Libro quitado de la colección" }
-            var diferencia: Int = 0
-            for (indiceLibro in librosQueQuitar){
-                conjunto.drop(indiceLibro-diferencia)
-                diferencia += 1
-            }
+            quitarDelConjunto(librosQueQuitar)
             return mensaje
         } else {
             return "No se ha encontrado ningún libro con el autor especificado"
         }
-
     }
-    fun mostrarExtremosCalificacion(){
-        val calificacionesLibros: List<Int> = listOf()
+    fun mostrarExtremosCalificacion(): String{
+        var resenasLibros: String = ""
+        val librosQueMostrar: MutableList<Libro> = mutableListOf()
+        val calificacionesLibros: MutableList<Int> = mutableListOf()
+        val libroToCalificacion: MutableMap<Libro, Int> = mutableMapOf()
         for (libro in conjunto){
-            calificacionesLibros.addLast(libro.calificacion())
+            calificacionesLibros.add(libro.calificacion())
+            libroToCalificacion += (libro to libro.calificacion())
         }
-       // var mayorCalificacion
-
+        val mayorCalificacion = calificacionesLibros.max()
+        val menorCalificacion = calificacionesLibros.min()
+        for((libro, calificacion) in libroToCalificacion){
+            if (calificacion == mayorCalificacion){
+                librosQueMostrar += libro
+            }
+            if (calificacion == menorCalificacion){
+                librosQueMostrar += libro
+            }
+        }
+        for (libro in librosQueMostrar){
+            resenasLibros += libro.toString()
+        }
+        return resenasLibros
+    }
+    override fun toString(): String {
+        var resenasLibros: String = ""
+        if (conjunto.isEmpty()){
+            return "No hay libros para mostrar"
+        } else {
+            for (libro in conjunto){
+                resenasLibros += libro.toString() + "\n"
+            }
+            return resenasLibros
+        }
     }
 }
 fun main() {
-    val papa: List<Int> = listOf(7, 1, 7, 2)
-    println(papa.max())
-    println(papa.min())
+    val librosDeMiInfancia = ConjuntoLibros()
+    val libroFantasia = Libro("Eragon", "Christopher Paolini", 544, 7)
+    val libroInfantil = Libro("Viaje al Reino de la Fantasía","Geronimo Stilton", 330, 5)
+    librosDeMiInfancia.anadirLibro(libroInfantil)
+    librosDeMiInfancia.anadirLibro(libroFantasia)
+    println(librosDeMiInfancia.toString())
+    println(librosDeMiInfancia.quitarPorAutor("Geronimo Stilton"))
+    println(librosDeMiInfancia.quitarPorTitulo("Eragon"))
+    val libroJuvenil = Libro("Eldest", "Christopher Paolini", 704, 9)
+    librosDeMiInfancia.anadirLibro(libroJuvenil)
+    println(librosDeMiInfancia.toString())
 }
