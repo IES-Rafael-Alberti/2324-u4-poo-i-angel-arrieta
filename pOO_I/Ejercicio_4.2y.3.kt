b579d97773065fb2fdce50abf033544fc30221c6
angel <aarrieta@iesrafaelalberti.es>
2024-01-22 19:12:57 +0100

    Ejercicio Robot + Cambios setup clases necesarias

--- pOO_I/Ejercicio_4.2y.3.kt
+++ pOO_I/Ejercicio_4.2y.3.kt
@@ -1,2 +1,31 @@
 package pOO_I
 
+class Persona (var peso: Double, var altura: Double){
+    var nombre: String? = null
+    var imc: Double = peso/(altura*altura)
+    constructor(nombre: String, peso: Double, altura: Double): this(peso, altura){
+        this.nombre = nombre
+    }
+    override fun toString(): String = "${nombre?: ""} Pesa $peso" + "kg y mide $altura" + "m. Tiene un IMC de $imc"
+}
+fun main(){
+    /*Ejercicio 4.2*/
+    val persona1 = Persona(85.4, 1.72)
+    val persona2 = Persona("José", 75.1, 1.61)
+    val persona3 = Persona("María", 62.9, 1.58)
+    println(persona1.toString())
+    println(persona2.toString())
+    println(persona3.toString())
+    /*
+    println("Nombre de la persona1: ")
+    var cambioNombre = readln()
+    if ((cambioNombre == "") or (false)){
+        cambioNombre = "Desconocido"
+    }
+    persona1.nombre = cambioNombre
+    println("Nombre: ${persona1.nombre}.\nPeso: ${persona1.peso}.\nAltura: ${persona1.altura}")
+    println("Persona3\nPeso: ${persona3.peso}.\nAltura: ${persona3.altura}.\nImc: ${persona3.imc}")
+    persona3.altura = 1.80
+    println("Persona3\nPeso: ${persona3.peso}.\nAltura: ${persona3.altura}.\nImc: ${persona3.imc}")
+    */
+}
---
+++ pOO_III/R2D2.kt
@@ -0,0 +1,66 @@
+package pOO_III
+
+enum class Direccion {
+    PositiveY, NegativeY, PositiveX, NegativeX
+}
+
+class Robot(var nombre: String) {
+    init {
+        require(nombre.isNotBlank()) { "El nombre del robot no puede estar vacío" }
+    }
+    private var posX: Int = 0
+
+    private var posY: Int = 0
+
+    private var direccion: Direccion = Direccion.PositiveY
+
+    fun mover(pasos: Array<Int>) {
+        for (paso in pasos) {
+            when (direccion) {
+                Direccion.PositiveX -> posX += paso
+                Direccion.NegativeX -> posX -= paso
+                Direccion.PositiveY -> posY += paso
+                Direccion.NegativeY -> posY -= paso
+            }
+            cambiarDireccion()
+        }
+    }
+
+    private fun cambiarDireccion() {
+        direccion = when (direccion) {
+            Direccion.PositiveY -> Direccion.NegativeX
+            Direccion.NegativeX -> Direccion.NegativeY
+            Direccion.NegativeY -> Direccion.PositiveX
+            Direccion.PositiveX -> Direccion.PositiveY
+        }
+    }
+
+    fun obtenerPosicion(): Pair<Int, Int> {
+        return Pair(posX, posY)
+    }
+
+    fun obtenerDireccion(): Direccion {
+        return direccion
+    }
+
+    override fun toString(): String {
+        return "$nombre está en (${obtenerPosicion().first}, ${obtenerPosicion().second}). Mirando en dirección ${obtenerDireccion()}"
+    }
+}
+
+fun main() {
+    val movimientos = arrayOf(
+        arrayOf<Int>(1, -5, 0, -9),
+        arrayOf<Int>(3, 3, 5, 6, 1, 0, 0, -7),
+        arrayOf<Int>(2, 1, 0, -1, 1, 1, -4),
+        arrayOf<Int>(),
+        arrayOf<Int>(3, 5)
+    )
+
+    val robot1 = Robot("R2D2")
+
+    for (movimiento in movimientos) {
+        robot1.mover(movimiento)
+        println(robot1)
+    }
+}
